550067a53f546570b51e62c59881a6b44c3479a2
Juliya Lipatova <575@ukr.net>
2025-04-12 20:41:33 +0200

    update searching title and text

--- entities.py
+++ entities.py
@@ -306,7 +306,7 @@ class Organizer(UserDict):
     def find_notes_by_text(self, text: str):
         found_notes=[]
         for _, note in self.notes.items():
-            search_phrase = f"{note.title.value} {note.value} {' '.join(note.tags)}".lower()
+            search_phrase = f"{note.title.value} {note.value}".lower()
             if text.lower() in search_phrase:
                 found_notes.append(note)
         if found_notes:
